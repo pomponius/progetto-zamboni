6ef7ac6a7e28414475efaf691f35bb64388efe33
lucasax <4eversax@gmail.com>
2015-04-24 18:11:41 +0200

    Revert "Revert "Sosptati segnali negati nel portmap""
    This reverts commit e932d0c2d3050fd2ed4a1e6e94de19df5321c82c.

--- subtractor_nbit.vhd
+++ subtractor_nbit.vhd
@@ -17,14 +17,15 @@ COMPONENT single_full_adder
 END COMPONENT;
 
 SIGNAL CARRY: STD_LOGIC_VECTOR(N DOWNTO 0);
-SIGNAL OV: STD_LOGIC;
+SIGNAL OV, B_N: STD_LOGIC;
 
 BEGIN
 
 CARRY(0)<='1';
 
 fulladd_gen: FOR I IN 0 TO (N-1) GENERATE
-	FF: single_full_adder PORT MAP (A(I), NOT B(I), CARRY(I), DIFF(I), CARRY(I+1));	
+  B_N <= NOT B(I);
+	FF: single_full_adder PORT MAP (A(I), B_N, CARRY(I), DIFF(I), CARRY(I+1));	
 END GENERATE;
 
 OV<=CARRY(N) XOR CARRY(N-1);
